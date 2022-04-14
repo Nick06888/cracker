50ae9accecb0cc002835f13eeae03e4ce68ccaf1
Nick06888 <75800411+Nick06888@users.noreply.github.com>
2022-04-14 17:14:39 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,15 @@
 @echo off
+color c
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Programma aperto come amministratore! Continuando...
+    goto continua
+) else (
+    echo Devi avviare il programma come amministratore!
+    pause
+)
+:continua
+bcdedit -set TESTSIGNING OFF
 cd dist
 explorer.exe %CD%
+pause
